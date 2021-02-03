f5e24c01857c6e3b4770b0ca121fdea102d35415
osaosemwen <osaosemwen@gmail.com>
2021-02-03 03:09:14 +0000

    simple code change

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Just testing World"}
+		res := &response{Message: "Hello Just testing the deployment"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
